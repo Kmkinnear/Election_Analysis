0058b03be6fd69b43d53ea914b4de34c0b4d2387
Kmkinnear <korykinnear@triad.rr.com>
2022-09-25 17:39:26 -0400

    Adding the election audit code.

---
+++ Python_Practice.py
@@ -0,0 +1,13 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+    
+for county, voters in counties_dict.items():
+
+        print(f"{county} County has {voters} registered voters")
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
+                {"county":"Denver", "registered_voters": 463353}, 
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county, voters in voting_data.values():
+        print(f'{county} County has {voters} registered voters')
---
+++ RPS_Unsolved.py
@@ -0,0 +1,10 @@
+
+word = "Peace"
+for letters in word:
+    print(letters)
+
+
+zoo = ["cow","dog", "snake","monkey"]
+for pet in zoo:
+    print(pet)
+    
